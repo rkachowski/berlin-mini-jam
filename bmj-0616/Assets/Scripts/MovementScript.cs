aa9a628f5bf72032218cdd45e7a60e956e93b63f
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-11 15:24:09 +0200

    movement script.

---
+++ bmj-0616/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+ public float speed  = 1.0f;
+ 
+ public void Update() {
+     var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+     transform.position += move * speed * Time.deltaTime;
+ }
+}
