5201a2cbb390990f92ed606fba1de6be47130445
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-11 22:04:12 +0200

    Try explosions.

--- bmj-0616/Assets/Scripts/GetAttractedScript.cs
+++ bmj-0616/Assets/Scripts/GetAttractedScript.cs
@@ -6,17 +6,21 @@ public class GetAttractedScript : MonoBehaviour
     [RangeAttribute(0.01f, 0.5f)]
     public float speed = 0.5f;
     private SpriteRenderer _renderer;
+    private Rigidbody2D _rigidbody;
+    private Hurtable _hurtable;
 
     public void Start()
     {
         AttractorScript script = (AttractorScript)Object.FindObjectOfType(typeof(AttractorScript));
         _attractor = script.gameObject;
         _renderer = (SpriteRenderer)GetComponent(typeof(UnityEngine.SpriteRenderer));
+		_rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _hurtable = gameObject.GetComponent<Hurtable>();
 
     }
     public void Update()
     {
-        if (AttractorScript.attracting)
+        if (AttractorScript.attracting && !_hurtable.isHurt())
         {
             var move = Vector3.MoveTowards(transform.position, _attractor.transform.position, speed);
             if (move.magnitude > 0.1)
@@ -24,6 +28,11 @@ public class GetAttractedScript : MonoBehaviour
                 _renderer.flipX = move.x - transform.position.x > 0;
 
                 transform.position = move;
+
+                _rigidbody.velocity = Vector3.zero;
+                _rigidbody.rotation = 0f;
+                _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
             }
         }
     }
--- bmj-0616/Assets/Scripts/Hurtable.cs
+++ bmj-0616/Assets/Scripts/Hurtable.cs
@@ -3,21 +3,36 @@ using System.Collections;
 
 public class Hurtable : MonoBehaviour {
 
+	public float hurtTimer = 0.8f;
+	public float counter ;
 	// Use this for initialization
 	void Start () {
-	
+		counter = hurtTimer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(counter < hurtTimer) counter += Time.deltaTime;
+
+	}
+
+	public bool isHurt()
+	{
+		return counter < hurtTimer;
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if(col.gameObject.GetComponent(typeof(Painful)))
 		{
-			Debug.Log("ow");
+			var vec = col.gameObject.transform.position - transform.position;
+
+			var rigidbody = gameObject.GetComponent<Rigidbody2D>();
+			rigidbody.AddForce(vec*3,ForceMode2D.Impulse);
+			rigidbody.constraints = RigidbodyConstraints2D.None;
+			rigidbody.AddTorque(Random.Range(0,10), ForceMode2D.Impulse);
+
+			counter = 0;
 		}
 	}
 }
