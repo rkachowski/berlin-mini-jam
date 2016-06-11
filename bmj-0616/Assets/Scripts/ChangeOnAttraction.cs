38928670046e73295ec0915813894222bd014668
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-11 17:44:19 +0200

    Change on attraction script.

---
+++ bmj-0616/Assets/Scripts/ChangeOnAttraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeOnAttraction : MonoBehaviour {
+    private SpriteRenderer _renderer;
+
+
+	public Sprite idle;
+	public Sprite active;
+    // Use this for initialization
+    void Start () {
+		_renderer = (SpriteRenderer)GetComponent(typeof(UnityEngine.SpriteRenderer));
+		_renderer.sprite = idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(AttractorScript.attracting)
+		{
+			_renderer.sprite = active;
+		}
+		else
+		{
+			_renderer.sprite = idle;
+		}
+	}
+}
