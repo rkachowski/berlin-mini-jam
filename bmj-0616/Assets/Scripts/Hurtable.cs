857eba0a4120a309a28b03232d4cb1af0921305a
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-11 18:19:50 +0200

    physics??

---
+++ bmj-0616/Assets/Scripts/Hurtable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hurtable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.GetComponent(typeof(Painful)))
+		{
+			Debug.Log("ow");
+		}
+	}
+}
