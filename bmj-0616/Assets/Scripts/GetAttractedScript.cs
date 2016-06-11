7018ef8d136f9d97ddaa505d56bb86e4ce56f799
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-06-11 18:02:53 +0200

    dog turns to face ya

--- bmj-0616/Assets/Scripts/GetAttractedScript.cs
+++ bmj-0616/Assets/Scripts/GetAttractedScript.cs
@@ -3,18 +3,28 @@ using UnityEngine;
 public class GetAttractedScript : MonoBehaviour
 {
     GameObject _attractor;
-    [RangeAttribute(0.01f,0.5f)]
+    [RangeAttribute(0.01f, 0.5f)]
     public float speed = 0.5f;
+    private SpriteRenderer _renderer;
+
     public void Start()
     {
         AttractorScript script = (AttractorScript)Object.FindObjectOfType(typeof(AttractorScript));
         _attractor = script.gameObject;
+        _renderer = (SpriteRenderer)GetComponent(typeof(UnityEngine.SpriteRenderer));
+
     }
     public void Update()
     {
         if (AttractorScript.attracting)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _attractor.transform.position, speed);
+            var move = Vector3.MoveTowards(transform.position, _attractor.transform.position, speed);
+            if (move.magnitude > 0.1)
+            {
+                _renderer.flipX = move.x - transform.position.x > 0;
+
+                transform.position = move;
+            }
         }
     }
 }
--- bmj-0616/Assets/Scripts/MovementScript.cs
+++ bmj-0616/Assets/Scripts/MovementScript.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
 
- public float speed  = 1.0f;
- 
- public void Update() {
-     var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-     transform.position += move * speed * Time.deltaTime;
- }
+    public float speed = 1.0f;
+    private SpriteRenderer _renderer;
+
+    void Start()
+    {
+        _renderer = (SpriteRenderer)GetComponent(typeof(UnityEngine.SpriteRenderer));
+    }
+
+    public void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        if (move.magnitude > 0.1)
+        {
+            _renderer.flipX = move.x < 0;
+            transform.position += move * speed * Time.deltaTime;
+        }
+    }
 }
